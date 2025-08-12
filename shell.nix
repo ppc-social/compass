bafdd440428aec72b33938a969f56730753cd0d4
melektron <matteo@elektron.work>
2025-08-12 13:49:49 +0200

    modified shell.nix to make automatic container management optional

--- shell.nix
+++ shell.nix
@@ -13,17 +13,21 @@
  shellHook = ''
   mkdir -p ./gitignore/mariadb-data
 
+  # prepare for docker use
   if command -V podman
   then
     DOCKER_CMD=podman
   elif command -V docker
   then
-    DOCKER_CMD=podman
+    DOCKER_CMD=docker
   else
     echo You need docker or podman installed on your system for this devshell to work
     exit 1
   fi
+  export DOCKER_CMD
+  export container_id=compass_app_dev_mariadb
 
+  # load dev environment
   if test -f .env.development
   then
     echo
@@ -31,24 +35,18 @@
     echo you need to create an .env.development file, containing your secrets/configs like seen in the .env.example file
     exit 1
   fi
-
   export $(cat .env.development | xargs)
   
-  # run mariadb
-  container_id=compass_app_dev_mariadb
-  $DOCKER_CMD create --rm -v ./gitignore/mariadb-data:/var/lib/mysql -e MARIADB_ROOT_PASSWORD=example -p 3306:3306 --name $container_id mariadb
-  echo "container id is: $container_id"
-
-  $DOCKER_CMD start $container_id
-
-  trap "$DOCKER_CMD stop $container_id" EXIT
-
   alias mariadb="podman exec -it compass_app_dev_mariadb mariadb -u root -pexample"
-
   alias run="poetry run python ./src/compass_app.py"
-
   alias python="poetry run python"
 
-
+  # launch db in shell if it isn't disabled (you may want to manage it manually if you use multiple shells)
+  if [ $DISABLE_DB_IN_SHELL != "true" ]; 
+  then 
+    bash ./dev/start_container.bash
+    # auto-stop when exiting
+    trap "bash ./dev/stop_container.bash" EXIT
+  fi
  '';
 }
